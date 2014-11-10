891a1915ea642eb78eb890eae402ba47743c1f37
Standa Fifik <standa.fifik@gmail.com>
2014-11-10 21:09:59 +0100

    initial documentation

---
+++ js/docs-setup.js
@@ -0,0 +1,116 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "analytics",
+      "shortName": "analytics",
+      "type": "overview",
+      "moduleName": "analytics",
+      "shortDescription": "Configuration",
+      "keywords": "analytics analyticsprovider angular api backends config configuration create currently dependencies desired ga gaprovider google mixpanel mixpanelprovider module overview sometoken supported token tracker ua-xxxxxxx-x"
+    },
+    {
+      "section": "api",
+      "id": "analytics.ga",
+      "shortName": "analytics.ga",
+      "type": "overview",
+      "moduleName": "analytics.ga",
+      "shortDescription": "Google Analytics module for angular-analytics",
+      "keywords": "analytics angular-analytics api ga google module overview"
+    },
+    {
+      "section": "api",
+      "id": "analytics.ga.ga",
+      "shortName": "analytics.ga.ga",
+      "type": "service",
+      "moduleName": "analytics.ga",
+      "shortDescription": "Service for Google Analytics backend.",
+      "keywords": "analytics api backend called directly ga gaprovider google service"
+    },
+    {
+      "section": "api",
+      "id": "analytics.ga.gaProvider",
+      "shortName": "analytics.ga.gaProvider",
+      "type": "service",
+      "moduleName": "analytics.ga",
+      "shortDescription": "Provider for setting analytics credentials",
+      "keywords": "analytics analytics-ga-gaprovider-page angular api class config credentials ga gaprovider google identifier js module mymodule provider service setting token ua-xxxx-x"
+    },
+    {
+      "section": "api",
+      "id": "analytics.mixpanel",
+      "shortName": "analytics.mixpanel",
+      "type": "overview",
+      "moduleName": "analytics.mixpanel",
+      "shortDescription": "Mixpanel module for angular-analytics",
+      "keywords": "analytics angular-analytics api mixpanel module overview"
+    },
+    {
+      "section": "api",
+      "id": "analytics.mixpanel.mixpanel",
+      "shortName": "analytics.mixpanel.mixpanel",
+      "type": "service",
+      "moduleName": "analytics.mixpanel",
+      "shortDescription": "Service for Mixpanel backend.",
+      "keywords": "analytics api backend called directly mixpanel mixpanelprovider service"
+    },
+    {
+      "section": "api",
+      "id": "analytics.mixpanel.mixpanelProvider",
+      "shortName": "analytics.mixpanel.mixpanelProvider",
+      "type": "service",
+      "moduleName": "analytics.mixpanel",
+      "shortDescription": "Provider for setting mixpanel credentials",
+      "keywords": "analytics analytics-mixpanel-mixpanelprovider-page api class credentials identifier mixpanel mixpanelprovider provider service setting token"
+    },
+    {
+      "section": "api",
+      "id": "analytics.tracker",
+      "shortName": "analytics.tracker",
+      "type": "service",
+      "moduleName": "analytics",
+      "shortDescription": "Wrapper for web analytics trackers",
+      "keywords": "$locationchangesuccess $on $rootscope $scope action additional analytics api automatically call called category click controller creates data event floor function generic identifier informations lang location locationchanged log logs manually math method network ngroute object pageview path process random router service session session- simple social socialbtnclicked startsession track tracker trackers translations type unique visit web wrapper"
+    },
+    {
+      "section": "api",
+      "id": "analytics.trackerProvider",
+      "shortName": "analytics.trackerProvider",
+      "type": "service",
+      "moduleName": "analytics",
+      "keywords": "analytics analytics-trackerprovider-page api backends class enabled list service trackerprovider"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {}
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js"
+  ]
+};
